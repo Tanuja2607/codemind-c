8aee3bacfcca406b570968d58c797757bf4d94c9
Tanuja2607 <147791935+Tanuja2607@users.noreply.github.com>
2023-11-24 10:21:27 +0530

    #include<stdio.h> #include<math.h> int main() {     int t;     for(t=1;t<=1;t++)     {     int a,b,c,d,i;     scanf("%d%d",&a,&b);     for(i=a+1;i<b;i++)     {     c=pow(i,2);     d=pow(i,3);         printf("%d %d %d ",i,c,d);     }     }
    }

---
+++ N_-_S_-_C.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t;
+    for(t=1;t<=1;t++)
+    {
+    int a,b,c,d,i;
+    scanf("%d%d",&a,&b);
+    for(i=a+1;i<b;i++)
+    {
+    c=pow(i,2);
+    d=pow(i,3);
+        printf("%d %d %d
+",i,c,d);
+    }
+    }
+    
+}
