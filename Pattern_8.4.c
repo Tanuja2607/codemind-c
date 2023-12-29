ec031acaa7de33eeb807424635aea8ca4ee971f2
Tanuja2607 <147791935+Tanuja2607@users.noreply.github.com>
2023-12-29 09:51:39 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=0;i<n;i++)     {         for(j=0;j<n;j++)       {         if(i==j||i+j==n-1)             printf("%c",i+65);             else             printf(" ");       }         printf(" "); } }

--- Pattern_8.4.c
+++ Pattern_8.4.c
@@ -1,19 +1,18 @@
-#include<stdio.h>
+   #include<stdio.h>
 int main()
 {
     int i,j,n;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     {
-        for(j=1;j<=n;j++)
-        {
-            if(i==j||i+j==n+1)
-            printf("%c",i+64);
+        for(j=0;j<n;j++)
+      {
+        if(i==j||i+j==n-1)
+            printf("%c",i+65);
             else
             printf(" ");
-        }
+      }
         printf("
 ");
-    }
-return 0;
+}
 }
