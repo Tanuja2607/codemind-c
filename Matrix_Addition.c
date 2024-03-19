942fbce1140115c25446ce1311c63138175a50f9
Tanuja2607 <147791935+Tanuja2607@users.noreply.github.com>
2024-03-19 15:09:07 +0530

    #include<stdio.h> int main() {     int r1,c1;     scanf("%d%d",&r1,&c1);     int arr[r1][c1],i,j;     for(i=0;i<r1;i++)     {         for(j=0;j<c1;j++)         {         scanf("%d",&arr[i][j]);         }     }     int r2,c2;     scanf("%d%d",&r2,&c2);     int brr[r2][c2];     for(i=0;i<r2;i++)     {         for(j=0;j<c2;j++)         {             scanf("%d",&brr[i][j]);         }     }     if(r1==r2 && c1==c2)     {         for(i=0;i<r1;i++)         {             for(j=0;j<c1;j++)             {                 printf("%d ",arr[i][j]+brr[i][j]);             }             printf(" ");         }
        }
        
    }

--- Matrix_Addition.c
+++ Matrix_Addition.c
@@ -1,33 +1,38 @@
 #include<stdio.h>
 int main()
 {
-    int r,c;
-    scanf("%d%d",&r,&c);
-    int a[r][c],i,j;
-    for(i=0;i<r;i++)
+    int r1,c1;
+    scanf("%d%d",&r1,&c1);
+    int arr[r1][c1],i,j;
+    for(i=0;i<r1;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<c1;j++)
         {
-            scanf("%d",&a[i][j]);
+        scanf("%d",&arr[i][j]);
         }
     }
     int r2,c2;
     scanf("%d%d",&r2,&c2);
-    int b[r2][c2];
+    int brr[r2][c2];
     for(i=0;i<r2;i++)
-    {
-      for(j=0;j<c2;j++)
-      {
-          scanf("%d",&b[i][j]);
-      }
-    }
-    for(i=0;i<r;i++)
     {
         for(j=0;j<c2;j++)
         {
-            printf("%d ",a[i][j]+b[i][j]);
+            scanf("%d",&brr[i][j]);
         }
-        printf("
+    }
+    if(r1==r2 && c1==c2)
+    {
+        for(i=0;i<r1;i++)
+        {
+            for(j=0;j<c1;j++)
+            {
+                printf("%d ",arr[i][j]+brr[i][j]);
+            }
+            printf("
 ");
-}
+        }
+        
+    }
+    
 }
